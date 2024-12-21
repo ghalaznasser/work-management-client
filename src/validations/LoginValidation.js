dbfdf8eb4c17d39a1d2fbebff8524bfca741dfb8
ghalaznasser <ghalanasser29@gmail.com>
2024-12-21 17:17:23 +0400

    Update LoginValidation.js

--- src/validations/LoginValidation.js
+++ src/validations/LoginValidation.js
@@ -6,6 +6,14 @@ const ValidationLoginSchema = Yup.object().shape({
         "Username can only contain letters and numbers, no special characters."
       ),
     password:Yup.string().min(8,"Passwore must be at least 8 characters !!")
+        .matches(
+
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+
+    "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character!"
+
+  )
+ 
     .required("Password is required !!"),
 })
 
